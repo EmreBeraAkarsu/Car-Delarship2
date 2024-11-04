01a30c9db8ed30d3a3f029a4c348838c4f8ea95c
Emre Akarsu <akarsutech@gmail.com>
2024-11-04 01:44:25 -0500

    Created the basics of the SalesContract class

--- src/main/java/com/pluralsight/SalesContract.java
+++ src/main/java/com/pluralsight/SalesContract.java
@@ -1,4 +1,57 @@
 package com.pluralsight;
 
 public class SalesContract {
+    private double salesTax = .5;
+    private double recordingFee = 100;
+    private double processingFee;
+    private boolean isFinance;
+
+    private double totalPrice;
+    private double monthlyPayment;
+
+    public SalesContract(boolean isFinance) {
+        this.isFinance = isFinance;
+    }
+
+    public double getSalesTax() {
+        return salesTax;
+    }
+
+    public void setSalesTax(double salesTax) {
+        this.salesTax = salesTax;
+    }
+
+    public double getRecordingFee() {
+        return recordingFee;
+    }
+
+    public void setRecordingFee(double recordingFee) {
+        this.recordingFee = recordingFee;
+    }
+
+    public double getProcessingFee() {
+        return processingFee;
+    }
+
+    public void setProcessingFee(double processingFee) {
+        this.processingFee = processingFee;
+    }
+
+    public boolean isFinance() {
+        return isFinance;
+    }
+
+    public void setFinance(boolean finance) {
+        isFinance = finance;
+    }
+
+    public double getTotalPrice(){
+
+
+    }
+
+    public double getMonthlyPayment(){
+
+
+    }
 }
